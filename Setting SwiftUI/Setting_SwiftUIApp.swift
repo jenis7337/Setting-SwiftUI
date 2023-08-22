ca7faf561841de44f23d30d45694bd9f673f496f
Jenis Navadiya <jenisnavadiya04@gmail.com>
2023-08-22 14:49:09 +0530

    Initial Commit

---
+++ Setting SwiftUI/Setting_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Setting_SwiftUIApp.swift
+//  Setting SwiftUI
+//
+//  Created by jenish navadiya on 22/08/23.
+//
+
+import SwiftUI
+
+@main
+struct Setting_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
